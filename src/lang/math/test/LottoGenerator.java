7b071fe7952bf04f7ba553a210579a8e427f6e99
SonYJ <soja0529@gmail.com>
2025-01-30 10:33:15 +0900

    김영한 중급1-4 문제와 풀이2 : 로또 번호 자동 생성기

--- src/lang/math/test/LottoGenerator.java
+++ src/lang/math/test/LottoGenerator.java
@@ -4,19 +4,31 @@ import java.util.Random;
 
 public class LottoGenerator {
     private Random random = new Random();
-    private int[] lottoArr = {};
+    private int[] lottoArr = new int[6];
 
     public void lottoPrint() {
-        lonttoGenerate();
+        lottoGenerate();
 
-        System.out.println("로또 번호 : " + lottoArr[0] + lottoArr[1] + lottoArr[2] + lottoArr[3] +
-                lottoArr[4] + lottoArr[5] + lottoArr[6]);
+        // System.out.println("로또 번호 : " + lottoArr[0] + lottoArr[1] + lottoArr[2] +
+        // lottoArr[3] + lottoArr[4] + lottoArr[5]);
+        // System.out.println(lottoArr[0]);
+
+        System.out.println("로또 번호 : " + lottoArr[0] + " " + lottoArr[1] + " " + lottoArr[2] + " " +
+                lottoArr[3] + " " + lottoArr[4] + " " + lottoArr[5]);
     }
 
-    private void lonttoGenerate() {
+    // 무작위 숫자 생성 로직
+    private void lottoGenerate() {
 
         for (int i = 0; i < 6; i++) {
             lottoArr[i] = random.nextInt(45) + 1;
+
+            // 중복 방지 로직
+            for (int j = 0; j < i; j++) {
+                if (lottoArr[i] == lottoArr[j]) {
+                    i -= 1;
+                }
+            }
         }
     }
 
