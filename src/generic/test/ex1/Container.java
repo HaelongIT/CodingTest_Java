09b4d6d52f7aface2df77a4b83f2737e07aab8b5
SonYJ <soja0529@gmail.com>
2025-02-21 10:56:27 +0900

    김영한 중급2-1 문제1 : 제네릭 기본1(완)

---
+++ src/generic/test/ex1/Container.java
@@ -0,0 +1,21 @@
+package generic.test.ex1;
+
+public class Container<T> {
+
+    private T item;
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return this.item;
+    }
+
+    public boolean isEmpty() {
+        if (item == null) {
+            return true;
+        }
+        return false;
+    }
+}
